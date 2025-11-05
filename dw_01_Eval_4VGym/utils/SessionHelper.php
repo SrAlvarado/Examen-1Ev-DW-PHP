a00a53fd425440b7c8d8a7e6be9a9eea6163bdb9
Markel Alvarado <markel.alvarado@gmail.com>
2025-11-05 09:32:13 +0100

    Creacion de SessionHelper que gestiona la última página vista para la redirección inicial

---
+++ dw_01_Eval_4VGym/utils/SessionHelper.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace utils;
+class SessionHelper
+{
+
+    private const LAST_PAGE_KEY = 'last_viewed_page';
+
+    private const DEFAULT_PAGE = 'listActivities.php';
+
+    public const LIST_PAGE = 'listActivities.php';
+    public const CREATE_PAGE = 'createActivity.php';
+
+    public static function startSession(): void
+    {
+        // Verifica si el estado de la sesión es PHP_SESSION_NONE (no iniciada)
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    public static function setLastViewedPage(string $pagePath): void
+    {
+        self::startSession();
+        // Guardamos el nombre de la página
+        $_SESSION[self::LAST_PAGE_KEY] = $pagePath;
+    }
+
+    public static function getRedirectPage(): string
+    {
+        self::startSession();
+
+        if (isset($_SESSION[self::LAST_PAGE_KEY]) && !empty($_SESSION[self::LAST_PAGE_KEY])) {
+            return $_SESSION[self::LAST_PAGE_KEY];
+        } else {
+            return self::DEFAULT_PAGE;
+        }
+    }
+}
