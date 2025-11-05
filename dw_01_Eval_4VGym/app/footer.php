8b60ee2b22fdcf3e78512cd64fd2efc7aabfea8f
Markel Alvarado <markel.alvarado@gmail.com>
2025-11-05 09:34:32 +0100

    Creacion de footer.php que crea el pie de página con copyright y scripts de Bootstrap

---
+++ dw_01_Eval_4VGym/app/footer.php
@@ -0,0 +1,15 @@
+<?php
+?>
+</div> <footer class="container-fluid mb-0" role="footer">
+    <div class="row">
+        <div class="col-lg-12">
+            <p>Copyright &copy; M. G. | 4VGym</p>
+        </div>
+    </div>
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
